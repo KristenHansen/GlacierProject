791128d452d166fcff3fea68e1c2fda62e1cf402
kahansen <k1hansen@ucsd.edu>
2019-11-19 20:47:38 -0800

    Reorganization of landsat download function

---
+++ landsatandglacierobject.py
@@ -0,0 +1,235 @@
+#Reorganize landsat download function
+def ee_download(glacierID, glacierObject, begDate = '1984-01-01', endDate = '2019-01-01', cloud_tol = 15, landsat = True, dem = True, outputObject = True):
+    #Inner function for computing cloud score such that we can remove bad images from the landsat collections we download
+    #Implementation in javascript can be found of Google Earth Engine website under (landsat algorithms), translation to python by KH
+    #further help from Nicholas Clinton at https://gis.stackexchange.com/questions/252685/filter-landsat-images-base-on-cloud-cover-over-a-region-of-interest
+    def cloudscore(image):
+        cloud = ee.Algorithms.Landsat.simpleCloudScore(image).select('cloud')
+        cloudiness = cloud.reduceRegion(ee.Reducer.mean(),
+                                        geometry=region,
+                                        scale=30)
+        image = image.set(cloudiness)
+        return image
+
+    #Modules needed import, csv and pydrive for sending glacier object to drive
+    #ee for download
+    from datetime import date
+    import csv
+    from pydrive.auth import GoogleAuth
+    from pydrive.drive import GoogleDrive
+    import os
+    import ee
+    #Initial earth engine connection, key much be on your computer, thus you must once in terminal run ee.Authenticate()
+    #for any new computer that you are using for implementation of this google earth engine communication
+    try:
+        ee.Initialize()
+        print('The Earth Engine package initialized successfully!')
+    except ee.EEException as e:
+        print('The Earth Engine package failed to initialize!')
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
+
+    #Our glacier region can be found in the imported dictionary as an argument under bounding box (list of lists of coordinates)
+    #We must create a gee polygon in order to use that to clip the images
+    region = ee.Geometry.Polygon(glacierObject['boundingbox'])
+    #Dummy request to Earth engine to compute glacier object values and send to toDrive
+    #Creates image collections for later batch export
+    #Landsat 8 image collection
+    if date.fromisoformat(endDate) > date.fromisoformat("2013-01-01"):
+        #First filter the collection of images by date and region of glacier
+        # Landsat 8 starts on 01-01-13
+
+        colL8 = ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA')
+        colL8 = colL8.filterDate('2013-01-01',endDate)
+        colL8 = colL8.filterBounds(region)
+        count = colL8.size()
+
+        #Filter out cloudiest images based on tolerance set as parameter
+        #We need bands 1-6
+        withCloudiness = colL8.map(algorithm = cloudscore)
+
+        filteredCollectionL8 = withCloudiness.filter(ee.Filter.lt('cloud', cloud_tol))
+        filteredCollectionL8 = filteredCollectionL8.select(['B1', 'B2', 'B3', 'B4', 'B5', 'B6'])
+
+        #In order to collect dates for object and pushing images to drive make our collection a list thus we can loop over
+        # and the size of the collection
+        collectionListL8 = filteredCollectionL8.toList(colL8.size())
+        collectionSizeL8 = collectionListL8.size().getInfo()
+        #Landsat 7 Image collection
+        #Same for the other landsats based on dates of start
+    if date.fromisoformat("1999-01-01") > date.fromisoformat(begDate):
+        colL7 = ee.ImageCollection('LANDSAT/LE07/C01/T1_TOA')
+        colL7 = colL7.filterDate(begDate,endDate)
+        colL7 = colL7.filterBounds(region)
+        count = colL7.size()
+
+
+    else:
+        colL7 = ee.ImageCollection('LANDSAT/LE07/C01/T1_TOA')
+        colL7 = colL7.filterDate('1999-01-01',endDate)
+        colL7 = colL7.filterBounds(region)
+        count = colL7.size()
+
+    withCloudiness = colL7.map(algorithm = cloudscore)
+
+    filteredCollectionL7 = withCloudiness.filter(ee.Filter.lt('cloud', cloud_tol))
+    filteredCollectionL7 = filteredCollectionL7.select(['B1', 'B2', 'B3', 'B4', 'B5', 'B6_VCID_1'])
+    collectionListL7 = filteredCollectionL7.toList(colL7.size())
+    collectionSizeL7 = filteredCollectionL7.size().getInfo()
+    #Landsat 5 image collection
+    if date.fromisoformat(endDate) < date.fromisoformat('2012-05-01'):
+        colL5 = ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA')\
+                .filterDate(begDate,endDate)\
+                .filterBounds(region)
+
+        count = colL5.size()
+    else:
+
+        colL5 = ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA')\
+                .filterDate(begDate,'2012-05-01')\
+                .filterBounds(region)
+
+        count = colL5.size()
+
+    withCloudiness = colL5.map(algorithm = cloudscore)
+
+    filteredCollectionL5 = withCloudiness.filter(ee.Filter.lt('cloud', cloud_tol))
+    filteredCollectionL5 = filteredCollectionL5.select(['B1', 'B2', 'B3', 'B4', 'B5', 'B6'])
+    collectionListL5 = filteredCollectionL5.toList(colL5.size())
+    collectionSizeL5 = filteredCollectionL5.size().getInfo()
+
+
+    #For each image collection we need the list of dates from that image as well as the sensor(to be added) it comes from, the following code extracts that information and adds to glacierObject
+    #Names of image attributes found at : https://developers.google.com/earth-engine/datasets/catalog/landsat
+    L8Dates = []
+    L7Dates = []
+    L5Dates = []
+    print("starting date collection")
+    #Loop over image collection lists made above and then get the date acquired and append to list
+    for i in range(collectionSizeL8):
+        image = ee.Image(collectionListL8.get(i)).clip(region)
+        date_acquired = image.get("DATE_ACQUIRED")
+        L8Dates.append(str(date_acquired.getInfo()))
+    print("L8 date collection complete")
+    for i in range(collectionSizeL7):
+        image = ee.Image(collectionListL7.get(i)).clip(region)
+        date_acquired = image.get("DATE_ACQUIRED")
+        L7Dates.append(str(date_acquired.getInfo()))
+    print("L7 date collection complete")
+    for i in range(collectionSizeL5):
+        image = ee.Image(collectionListL5.get(i)).clip(region)
+        date_acquired = image.get("DATE_ACQUIRED")
+        L5Dates.append(str(date_acquired.getInfo()))
+    print("l5 date collection complete")
+
+    #Add date lists to the glacier object
+    glacierObject['L8Dates'] = L8Dates
+    glacierObject['L7Dates'] = L7Dates
+    glacierObject['L5Dates'] = L5Dates
+    #The title of the folder where the glacier object and images will be located
+    glacierObject['fileaddress'] = str(glacierObject['GlimsID'])
+
+    #CSV implementation for making the dictionary a csv and then writing it such that it can be used to push to drive location
+    #Help found from code at: https://www.tutorialspoint.com/How-to-save-a-Python-Dictionary-to-CSV-file
+    print("Creating CSV")
+    csv_file = str(glacierObject['GlimsID']) + ".csv"
+    csv_columns = ['GlimsID', 'boundingbox','L8Dates','L7Dates','L5Dates', 'fileaddress']
+    with open(csv_file, "w") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames = csv_columns)
+        writer.writeheader()
+        writer.writerow(glacierObject)
+
+    print("Making google drive glacier object")
+    #Google Drive API implementation
+    # Getting google drive API to work found at: https://medium.com/@annissouames99/how-to-upload-files-automatically-to-drive-with-python-ee19bb13dda
+    # Uploading files code found at: https://developers.google.com/drive/api/v3/manage-uploads
+    # However that is where the help from developers group stopped helping, do not use it for creating folders and such, their solutions don't work with python API
+    #Or are outdated
+    g_login = GoogleAuth()
+    g_login.LocalWebserverAuth()
+    drive = GoogleDrive(g_login)
+
+    #Helpful links:
+    #https://github.com/gsuitedevs/PyDrive/issues/72
+    #https://towardsdatascience.com/how-to-manage-files-in-google-drive-with-python-d26471d91ecd
+    nameforfile = str(glacierObject['GlimsID']) + ".csv"
+    #Assign mime type such that it creates a file in drive
+    folder_metadata = {
+    'title': str(glacierObject['GlimsID']),
+    'mimeType': 'application/vnd.google-apps.folder'
+    }
+    folder = drive.CreateFile(folder_metadata)
+    folder.Upload()
+
+    #Need the id of the folder because google drive does not work like a normal file system
+    #operates on ID's found in metadata
+    folderid = folder['id']
+    print('Folder ID: %s' % folderid)
+
+    #Now that we created the folder we must create the file that will store the glacier object using the csv created above
+    # Then upload to drive
+    file = drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": folderid}]})
+    file.SetContentFile(str(glacierID) + ".csv")
+    file.Upload()
+
+    #file=drive.CreateFile(glacierObject)
+    #file.SetContentFile(str(glacierID) + ".csv")
+    #file.Upload()
+
+    #Delete the csv that was written to disk, and print the file does not exist if it doesn't
+    #Then we will know that and know that the glacier object was not correctly created
+    if os.path.exists(str(glacierID) + ".csv"):
+        os.remove(str(glacierID) + ".csv")
+    else:
+        print("The file does not exist")
+    print("glacier object uploaded to google drive")
+
+    #Now is the part behind the GEE server
+    #First the DEM
+    if dem == True:
+        #Create image from GEE and clip to our glacier region
+        DEM = ee.Image("USGS/SRTMGL1_003")
+        DEM = DEM.clip(region)
+        # export this image to drive in the glimsID folder specified in input object
+        task = ee.batch.Export.image.toDrive(
+            image = DEM.clip(region),
+            scale = 30, region = region.bounds().getInfo()['coordinates'], folder = str(glacierObject['GlimsID']), fileNamePrefix = 'USGS_DEM')
+        task.start()
+        print("dem sent to drive")
+    if landsat == True:
+        #We already created the image collections previously and now in a for loop we 
+        #batch export the images from the collections with the date as the name
+        #similarly for each of the three landsat collections
+        for i in range(collectionSizeL8):
+            image = ee.Image(collectionListL8.get(i)).clip(region)
+            filename = image.get("DATE_ACQUIRED")
+            task = ee.batch.Export.image.toDrive(
+                image = ee.Image(collectionListL8.get(i)).clip(region),
+                scale = 30, region = region.bounds().getInfo()['coordinates'], folder = str(glacierObject['GlimsID']), fileNamePrefix = str(filename.getInfo()))
+            task.start()
+        print(collectionSizeL8)
+        print("L8 images sent to drive")
+        for i in range(collectionSizeL7):
+            image = ee.Image(collectionListL7.get(i)).clip(region)
+            filename = image.get("DATE_ACQUIRED")
+            task = ee.batch.Export.image.toDrive(
+                image = ee.Image(collectionListL7.get(i)).clip(region),
+                scale = 30, region = region.bounds().getInfo()['coordinates'], folder = str(glacierObject['GlimsID']), fileNamePrefix = str(filename.getInfo()))
+            task.start()
+        print(collectionSizeL7)
+        print("L7 images sent to drive")
+        for i in range(collectionSizeL5):
+            image = ee.Image(collectionListL5.get(i)).clip(region)
+            filename = image.get("DATE_ACQUIRED")
+            task = ee.batch.Export.image.toDrive(
+                image = ee.Image(collectionListL5.get(i)).clip(region),
+                scale = 30, region = region.bounds().getInfo()['coordinates'], folder = str(glacierObject['GlimsID']), fileNamePrefix = str(filename.getInfo()))
+            task.start()
+        print(collectionSizeL5)
+        print("L5 images sent to drive")
+
+dct = dict()
+dct['GlimsID'] = "G098570E39226N"
+dct['boundingbox'] = [[98.54723199999999, 39.210651], [98.59022299999999, 39.210651], [98.59022299999999, 39.243147], [98.54723199999999, 39.243147], [98.54723199999999, 39.210651]]
+ee_download("G098570E39226N", dct, cloud_tol = 20, landsat = True, dem = True)
--- landsatdownload.py
+++ landsatdownload.py
@@ -16,7 +16,7 @@ def cloudscore(image):
     return image
 
 
-def ee_download(glacierID, glacierObject, begDate = '1984-01-01', endDate = '2019-01-01', cloud_tol = 15):
+def ee_download(glacierID, glacierObject, begDate = '1984-01-01', endDate = '2019-01-01', cloud_tol = 15, landsat = True, dem = True):
     #Earliest begin date is '1984-01-01'
     def cloudscore(image):
         import ee
@@ -36,6 +36,10 @@ def ee_download(glacierID, glacierObject, begDate = '1984-01-01', endDate = '201
         return image
 
     from datetime import date
+    import csv
+    from pydrive.auth import GoogleAuth
+    from pydrive.drive import GoogleDrive
+    import os
     import ee
     try:
         ee.Initialize()
@@ -48,108 +52,151 @@ def ee_download(glacierID, glacierObject, begDate = '1984-01-01', endDate = '201
     #Prepare for Landsat Download
     region = ee.Geometry.Polygon(glacierObject['boundingbox'])
     #DEM Download
-    DEM = ee.Image("USGS/SRTMGL1_003")
-    DEM = DEM.clip(region)
-    task = ee.batch.Export.image.toDrive(
-        image = DEM.clip(region),
-        scale = 30, region = region.bounds().getInfo()['coordinates'], folder = str(glacierObject['GlimsID']), fileNamePrefix = 'DEM')
-    task.start()
-    print("DEM sent to drive")
-
+    if dem == True:
+        DEM = ee.Image("USGS/SRTMGL1_003")
+        DEM = DEM.clip(region)
+        task = ee.batch.Export.image.toDrive(
+            image = DEM.clip(region),
+            scale = 30, region = region.bounds().getInfo()['coordinates'], folder = str(glacierObject['GlimsID']), fileNamePrefix = 'DEM')
+        task.start()
+        print("DEM sent to drive")
+    if landsat == True:
     #Landsat 8 download
-    if date.fromisoformat(endDate) > date.fromisoformat("2013-01-01"):
-        col = ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA')
-        col = col.filterDate('2013-01-01',endDate)
-        col = col.filterBounds(region)
-        count = col.size()
+        if date.fromisoformat(endDate) > date.fromisoformat("2013-01-01"):
+            col = ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA')
+            col = col.filterDate('2013-01-01',endDate)
+            col = col.filterBounds(region)
+            count = col.size()
+
+            withCloudiness = col.map(algorithm = cloudscore)
+
+            filteredCollection = withCloudiness.filter(ee.Filter.lt('cloud', cloud_tol))
+            filteredCollection = filteredCollection.select(['B1', 'B2', 'B3', 'B4', 'B5', 'B6'])
+            collectionList = filteredCollection.toList(col.size())
+            collectionSize = collectionList.size().getInfo()
+            L8Dates = []
+            for i in range(collectionSize):
+                image = ee.Image(collectionList.get(i)).clip(region)
+                filename = image.get("DATE_ACQUIRED")
+                L8Dates.append(str(filename.getInfo()))
+                task = ee.batch.Export.image.toDrive(
+                    image = ee.Image(collectionList.get(i)).clip(region),
+                    scale = 30, region = region.bounds().getInfo()['coordinates'], folder = str(glacierObject['GlimsID']), fileNamePrefix = str(filename.getInfo()))
+                task.start()
+            print("Landsat 8 images sent")
+            glacierObject['L8Dates'] = L8Dates
+        # Landsat 7 download
+        if date.fromisoformat("1999-01-01") > date.fromisoformat(begDate):
+            col2 = ee.ImageCollection('LANDSAT/LE07/C01/T1_TOA')
+            col2 = col2.filterDate(begDate,endDate)
+            col2 = col2.filterBounds(region)
+            count = col2.size()
+
+
+        else:
+            col2 = ee.ImageCollection('LANDSAT/LE07/C01/T1_TOA')
+            col2 = col2.filterDate('1999-01-01',endDate)
+            col2 = col2.filterBounds(region)
+            count = col2.size()
+
+        withCloudiness = col2.map(algorithm = cloudscore)
+
+        filteredCollection = withCloudiness.filter(ee.Filter.lt('cloud', cloud_tol))
+        filteredCollection = filteredCollection.select(['B1', 'B2', 'B3', 'B4', 'B5', 'B6_VCID_1'])
+        collectionList = filteredCollection.toList(col2.size())
+        collectionSize = filteredCollection.size().getInfo()
+        L7Dates = []
+        for i in range(collectionSize):
+            image = ee.Image(collectionList.get(i)).clip(region)
+            filename = image.get("DATE_ACQUIRED")
+            L7Dates.append(str(filename.getInfo()))
+            task = ee.batch.Export.image.toDrive(
+                image = ee.Image(collectionList.get(i)).clip(region),
+                scale = 30, region = region.bounds().getInfo()['coordinates'], folder = str(glacierObject['GlimsID']), fileNamePrefix = str(filename.getInfo()))
+            task.start()
+        print("Landsat 7 images sent")
+        glacierObject['L7Dates'] = L7Dates
+        #Landsat 5 download
+        if date.fromisoformat(endDate) < date.fromisoformat('2012-05-01'):
+            col = ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA')\
+                    .filterDate(begDate,endDate)\
+                    .filterBounds(region)
+
+            count = col.size()
+        else:
+
+            col = ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA')\
+                    .filterDate(begDate,'2012-05-01')\
+                    .filterBounds(region)
+
+            count = col.size()
 
         withCloudiness = col.map(algorithm = cloudscore)
 
         filteredCollection = withCloudiness.filter(ee.Filter.lt('cloud', cloud_tol))
         filteredCollection = filteredCollection.select(['B1', 'B2', 'B3', 'B4', 'B5', 'B6'])
-        collectionList = filteredCollection.toList(col.size())
-        collectionSize = collectionList.size().getInfo()
-
+        collectionList = filteredCollection.toList(col2.size())
+        collectionSize = filteredCollection.size().getInfo()
+        L5Dates = []
         for i in range(collectionSize):
             image = ee.Image(collectionList.get(i)).clip(region)
             filename = image.get("DATE_ACQUIRED")
-
-            #print(date.getInfo())
+            L5Dates.append(str(filename.getInfo()))
             task = ee.batch.Export.image.toDrive(
                 image = ee.Image(collectionList.get(i)).clip(region),
                 scale = 30, region = region.bounds().getInfo()['coordinates'], folder = str(glacierObject['GlimsID']), fileNamePrefix = str(filename.getInfo()))
             task.start()
-        print("Landsat 8 images sent")
-        glacierObject['L8Dates'] = filteredCollection.get("DATE_ACQUIRED").getInfo()
-    # Landsat 7 download
-    if date.fromisoformat("1999-01-01") > date.fromisoformat(begDate):
-        col2 = ee.ImageCollection('LANDSAT/LE07/C01/T1_TOA')
-        col2 = col2.filterDate(begDate,endDate)
-        col2 = col2.filterBounds(region)
-        count = col2.size()
+        print("Landsat 5 images sent")
+        glacierObject['L5Dates'] = L5Dates
 
 
-    else:
-        col2 = ee.ImageCollection('LANDSAT/LE07/C01/T1_TOA')
-        col2 = col2.filterDate('1999-01-01',endDate)
-        col2 = col2.filterBounds(region)
-        count = col2.size()
-
-    withCloudiness = col2.map(algorithm = cloudscore)
-
-    filteredCollection = withCloudiness.filter(ee.Filter.lt('cloud', cloud_tol))
-    filteredCollection = filteredCollection.select(['B1', 'B2', 'B3', 'B4', 'B5', 'B6_VCID_1'])
-    collectionList = filteredCollection.toList(col2.size())
-    collectionSize = filteredCollection.size().getInfo()
-
-    for i in range(collectionSize):
-        image = ee.Image(collectionList.get(i)).clip(region)
-        filename = image.get("DATE_ACQUIRED")
-        task = ee.batch.Export.image.toDrive(
-            image = ee.Image(collectionList.get(i)).clip(region),
-            scale = 30, region = region.bounds().getInfo()['coordinates'], folder = str(glacierObject['GlimsID']), fileNamePrefix = str(filename.getInfo()))
-        task.start()
-    print("Landsat 7 images sent")
-    glacierObject['L7Dates'] = filteredCollection.get("DATE_ACQUIRED").getInfo()
-    #Landsat 5 download
-    if date.fromisoformat(endDate) < date.fromisoformat('2012-05-01'):
-        col = ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA')\
-                .filterDate(begDate,endDate)\
-                .filterBounds(region)
-
-        count = col.size()
-    else:
-
-        col = ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA')\
-                .filterDate(begDate,'2012-05-01')\
-                .filterBounds(region)
-
-        count = col.size()
-
-    withCloudiness = col.map(algorithm = cloudscore)
-
-    filteredCollection = withCloudiness.filter(ee.Filter.lt('cloud', cloud_tol))
-    filteredCollection = filteredCollection.select(['B1', 'B2', 'B3', 'B4', 'B5', 'B6'])
-    collectionList = filteredCollection.toList(col2.size())
-    collectionSize = filteredCollection.size().getInfo()
-
-    for i in range(collectionSize):
-        image = ee.Image(collectionList.get(i)).clip(region)
-        filename = image.get("DATE_ACQUIRED")
-        task = ee.batch.Export.image.toDrive(
-            image = ee.Image(collectionList.get(i)).clip(region),
-            scale = 30, region = region.bounds().getInfo()['coordinates'], folder = str(glacierObject['GlimsID']), fileNamePrefix = str(filename.getInfo()))
-        task.start()
-    print("Landsat 5 images sent")
-    glacierObject['L5Dates'] = filteredCollection.get("DATE_ACQUIRED").getInfo()
-    #Need to add a list of all dates of landsats to glacier glacierObject
-    #Add an address of the files DEM and landsat
     glacierObject['fileaddress'] = str(glacierObject['GlimsID'])
-    
+
+
+    csv_file = str(glacierObject['GlimsID']) + ".csv"
+    csv_columns = ['GlimsID', 'boundingbox','L8Dates','L7Dates','L5Dates', 'fileaddress']
+    with open(csv_file, "w") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames = csv_columns)
+        writer.writeheader()
+        writer.writerow(glacierObject)
+
+
+
+    g_login = GoogleAuth()
+    g_login.LocalWebserverAuth()
+    drive = GoogleDrive(g_login)
+
+    file=drive.CreateFile(glacierObject)
+    file.SetContentFile(str(glacierID) + ".csv")
+    file.Upload()
+
+
 
 dct = dict()
+#Bayi
 dct['GlimsID'] = "G098570E39226N"
 dct['boundingbox'] = [[98.54723199999999, 39.210651], [98.59022299999999, 39.210651], [98.59022299999999, 39.243147], [98.54723199999999, 39.243147], [98.54723199999999, 39.210651]]
+ee_download("G098570E39226N", dct, cloud_tol = 20, landsat = True, dem = True)
+
+#McCall Glacier
+# dct['GlimsID'] = "G216152E69302N"
+# dct['boundingbox'] = [[-143.860976, 69.276937],[-143.779992, 69.276937],[-143.779992, 69.334028],[-143.860976, 69.334028]]
+
+#noName
+# dct['GlimsID'] = "G073183E39306N"
+# dct['boundingbox'] = [[73.169516, 39.297678], [73.191774, 39.297678], [73.191774, 39.316767], [73.169516, 39.316767], [73.169516, 39.297678]]
+# ee_download("G073183E39306N", dct, cloud_tol = 20, landsat = True, dem = True)
+
+# ee_download("G216152E69302N", dct, cloud_tol = 20, landsat = False, dem = False)
 
 
-ee_download("G098570E39226N", dct, cloud_tol = 20)
+
+#Name CN5Y725D0005
+# dct['GlimsID'] = "G088313E43812N"
+# dct['boundingbox']=[[88.274114, 43.789814], [88.341902, 43.789814], [88.341902, 43.826003], [88.274114, 43.826003], [88.274114, 43.789814]]
+# ee_download("G088313E43812N", dct, cloud_tol = 20, landsat = False, dem = False)
+
+#Names CN5Y730C0029 Urumqi Source Glacier No1 W Br
+# dct['GlimsID'] = "G086801E43117N"
+# dct['boundingbox'] = [[86.795518, 43.110825],[86.81219299999999, 43.110825], [86.81219299999999, 43.120259], [86.795518, 43.120259], [86.795518, 43.110825]]
+# ee_download("G086801E43117N", dct, cloud_tol = 20, landsat = True, dem = True)
